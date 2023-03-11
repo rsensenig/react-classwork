16f00d95bd9740ffabedd4202d3db822f7b264fa
rsensenig <r.a.sensenig@gmail.com>
2023-03-11 13:03:38 -0500

    create a list of influencers with information about where they live and their social engagement stats

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 
+const influencerList = [
+  {
+    id: 1,
+    name: 'Rachel Sensenig',
+    location: 'Somerville, MA',
+    followers: 768,
+    following: 59
+  },
+  {
+    id: 2,
+    name: 'Kit Fenrir',
+    location: 'Denver, CO',
+    followers: 5478,
+    following: 148
+  }
+]
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>List of Influencers:</h1>
+      <InfluencerInfoItem />
     </div>
   );
+}
+
+function InfluencerInfoItem() {
+  return (
+    influencerList.map(influencer => (
+      <ul key= {influencer.id}>
+        <li>Name: {influencer.name}</li>
+        <li>Location: {influencer.location}</li>
+        <li>Followers: {influencer.followers}</li>
+        <li>Following: {influencer.following}</li>
+      </ul>
+    ))
+  )
 }
 
 export default App;
