be287ba33eeaac3f82213cce44c623d96db08f4c
rsensenig <r.a.sensenig@gmail.com>
2023-03-20 21:42:26 -0400

    create a new state for users, create a useEffect for getting users asynchronously, create a state for loading the user data, and use conditional rendering to show when user data is loading

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,4 +1,15 @@
-export const UserForm = ({userName, handleUserNameChange}) => {
+import { useRef, useEffect } from "react"
+
+export const UserForm = ({userName, handleUserNameChange, isFocused}) => {
+    const inputRef = useRef();
+
+    // use an if statement to decide whether the focus of inputRef is focused
+    useEffect(() => {
+        if(isFocused && inputRef.current) {
+            inputRef.current.focus()
+        }
+    }, [isFocused])
+    
     return (
     <div>
         <label htmlFor="username">New User:</label>
@@ -8,6 +19,7 @@ export const UserForm = ({userName, handleUserNameChange}) => {
             type="text"
             value={userName}
             onChange={handleUserNameChange}
+            ref={inputRef}
         />
         <p>Searching for a new user name: {userName}</p>
     </div>
--- src/components/UserItem.js
+++ src/components/UserItem.js
@@ -1,11 +1,20 @@
-export const UserItem = ({userList}) => {
+export const UserItem = ({initialUsers, handleUserInformation}) => {
     return (
-        userList.map(user => (
+        initialUsers.map(user => (
             <ul key= {user.id}>
                 <li>Name: {user.name}</li>
                 <li>Location: {user.location}</li>
                 <li>Followers: {user.followers}</li>
                 <li>Following: {user.following}</li>
+                <li>
+                    <button
+                        type="button"
+                        id="btnEditUser"
+                        onClick={handleUserInformation}
+                    >
+                    Edit User
+                    </button>
+                </li>
             </ul>
         ))
     )
