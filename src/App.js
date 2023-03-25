989c01a40acdde479ae798388e04d18ab17d72f4
rsensenig <r.a.sensenig@gmail.com>
2023-03-25 13:59:41 -0400

    replace user data from jsonplaceholder with user data from GitHub by using axios API

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import { UserItem } from './components/UserItem';
-import { UserForm } from './components/UserForm';
-
-// const initialUsers = [
-//   {
-//     id: 1,
-//     name: 'Rachel Sensenig',
-//     location: 'Somerville, MA',
-//     followers: 768,
-//     following: 59
-//   },
-//   {
-//     id: 2,
-//     name: 'Kit Fenrir',
-//     location: 'Denver, CO',
-//     followers: 5478,
-//     following: 148
-//   }
-// ]
+import { UserItem } from './User/UserItem';
+// import { UserForm } from './components/UserForm';
+import InputField from './shared/InputField';
+import axios from 'axios';
 
 function App() {
   // create a new state named 'users', and initialize with an empty array
   const [users, setUsers] = useState([]);
   
   // Create a new state and its setter function. This state will keep the value of the user input via input field. The value will represent a username.
-  // Initial value of the state is an empty string
-  const [userName, setUserName] = useState('');
+  // Initial value of the state is your GitHub username
+  const [userName, setUserName] = useState('rsensenig');
 
   // create a new state named 'isLoading', initialize to false
   const [isLoading, setIsLoading] = useState(false);
@@ -35,75 +20,61 @@ function App() {
   useEffect(() => {
     localStorage.setItem("Username:", userName)
   }, [userName])
-  
-  useEffect(() => {
-    // use the setter method for 'isLoading' and set it to 'true'
-    setIsLoading(true);
-    
-    // start a fetch call
-    fetch("https://jsonplaceholder.typicode.com/users")
-      // take a response and convert it into a json file
-      .then(res => res.json())
-      // take the data and using the setter function of the 'users' state, update the 'users' state
-      // set isLoading to false again
-      .then(res => {
-        setUsers(res)
-        setIsLoading(false)
-      }
-      )
 
-    // in the callback function of the useEffect, call a function called getUsersAsync
-    // getUsersAsync()
-    // // attach a then chain to the getUsersAsync function call
-    // // return a response
-    //   .then(res => {
-    //     // use the setter function for 'users' state to set the 'users' to the response data
-    //     setUsers(res.data);
-    //     // set isLoading back to false
-    //     setIsLoading(false);
-    //   })
-
-    // leave the dependency array empty
-  }, [])
-  
-  // define getUsersAsync
-  // const getUsersAsync = () => {
-  //   // return a new promise
-  //   return new Promise(resolve => {
-  //     // create a setTimeout to resolve the data after 3 seconds
-  //     setTimeout(() => resolve({data: initialUsers}), 3000)
-  //   })
-  // }
-
-  // handler function
+  // handler function for username change
   const handleUserNameChange = (event) => {
-    console.log(event.target.value);
     // set the state that will hold the user name to the value entered into the field
     setUserName(event.target.value);
   }
   
   // handler function to display more information about each user
-  const handleUserInformation = () => {
-    console.log(userName);
+  const handleUserDisplay = (user) => {
+    console.log(`You selected ${user.name}`);
+  }
+
+  // handler function to make API call after form submission
+  const handleCallUserApi = (event) => {
+    // prevent page refresh
+    event.preventDefault()
+
+     // use the setter method for 'isLoading' and set it to 'true'
+     setIsLoading(true);
+    
+     // start a fetch call
+     axios.get(`https://api.github.com/users/${userName}`)
+       // take the data and using the setter function of the 'users' state, update the 'users' state
+       // set isLoading to false again
+       .then(response => {
+         setUsers([response.data])
+         setIsLoading(false)
+       }
+       )
   }
 
   return (
     <div className="App">
-      <h1>Enter a New User:</h1>
-      <UserForm 
-        userName={userName}
-        handleUserNameChange={handleUserNameChange}
-        isFocused
-      />
-      <br />
-      <h1>List of Users:</h1>
+      <h1>Users</h1>
+      
+      <form onSubmit={handleCallUserApi}>
+        <InputField
+          id="user-name"
+          type="text"
+          value={userName}
+          onChangeFunction={handleUserNameChange}
+          isFocused
+        >
+          User:
+        </InputField>
+        <button type="submit">Search</button>
+      </form>
+
       {/* use conditional rendering to render either a p tag with text "Loading..." if isLoading is true, OR the UserItem component if it is false */}
       {isLoading ? (
         <p>Loading...</p>
       ) : (
         <UserItem 
         users={users}
-        handleUserInformation={handleUserInformation}
+        handleUserDisplay={handleUserDisplay}
       />
       )}
     </div>
